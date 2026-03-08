712900d883a0c2579d97e968b4c553f21263799d
Uday Gadhavi <udaygadhavi2410@gmail.com>
2026-03-08 17:03:09 +0530

    Fixed auth method syntax in protectRoute

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -5,7 +5,7 @@ export const protectRoute = [
   requireAuth(), // Ensure the user is authenticated with Clerk
   async (req, res, next) => {
     try {
-      const clerkId = req.auth.userId; // Get the authenticated user's Clerk ID
+      const clerkId = req.auth().userId; // Get the authenticated user's Clerk ID
       const user = await User.findOne({ clerkId }); // Find the user in MongoDB using the Clerk ID
       if (!user) {
         return res.status(404).json({ message: "User not found" }); // If user is not found, return 404
